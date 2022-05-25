b98f89e232aa954b763d24f79969a3aee9f33287
Kai Bellinghausen <bellinghausen.kai@gmail.com>
2022-05-25 11:19:31 +0200

    build: adds confusion matrix metrics

--- models/evaluation.py
+++ models/evaluation.py
@@ -1,22 +1,23 @@
 #---
 # Modules
 #---
-from matplotlib.pyplot import tight_layout
 import numpy as np
 
 #---
 # Graphical evaluation
 #---
-def plot_cf(cf_matrix):
+def plot_cf(model, X_test, y_test,):
     """
     Description:
-        Plots a 2x2 Confusion Matrix labeled with percentages and absolute values
+        Plots a 2x2 Confusion Matrix labeled with rates and absolute values
 
     Parameters:
-        cf_matrix (np.array): confusion matrix generated with sklearn.metrics.confusion_matrix. Shape(2,2)
+        model (RandomForestClassifier): Fitted model (RF).
+        X_test (np.array): Predictors of test data. Shape(time, features)
+        y_test (np.array): Predictand of test data. Shape(time,)
 
     Returns:
-        fig (matplotlib.figure.Figure): Figure of labeled confusion matrix
+        fig (matplotlib.figure.Figure): Figure of labeled confusion matrix. Percentages are rates (False Positive Rate etc.)
     
     Source (adjusted):
         "https://www.stackvidhya.com/plot-confusion-matrix-in-python-and-why/"
@@ -26,6 +27,11 @@ def plot_cf(cf_matrix):
     #---
     import seaborn as sns
     import matplotlib.pyplot as plt
+    from sklearn.metrics import confusion_matrix
+
+    # Get confusion matrix
+    y_test_pred = model.predict(X_test)
+    cf_matrix = confusion_matrix(y_test, y_test_pred)
 
     # Labels for quadrants in matrix
     group_names = ['True Neg','False Pos','False Neg','True Pos']
@@ -33,11 +39,18 @@ def plot_cf(cf_matrix):
     group_counts = ["{0:0.0f}".format(value) for value in
                     cf_matrix.flatten()]
 
-    group_percentages = ["{0:.2%}".format(value) for value in
-                        cf_matrix.flatten()/np.sum(cf_matrix)]
+    # Calculate Rates of Matrix
+    tn, fp, fn, tp = cf_matrix.ravel()
+    tnr = tn / (tn + fp) # True negative rate
+    fpr = 1 - tnr # False Positive Rate
+    tpr = tp / (tp + fn) # True positive rate
+    fnr = 1 - tpr # False negative rate
+    values = [tnr, fpr, fnr, tpr,]
+
+    rates = ["{0:.2%}".format(value) for value in values]
 
     labels = [f"{v1}\n{v2}\n{v3}" for v1, v2, v3 in
-            zip(group_names, group_counts, group_percentages)]
+            zip(group_names, group_counts, rates)]
 
     labels = np.asarray(labels).reshape(2,2)
 
@@ -60,3 +73,46 @@ def plot_cf(cf_matrix):
     ax.yaxis.set_ticklabels(['0','1'])
 
     return fig
+
+#---
+# Metrics
+#---
+def cfm_metrics(model, X_test, y_test, beta=0.5):
+    """
+    Description:
+        Use standard metrics for random forest classification with confusion matrix.
+    Parameters:
+        model (RandomForestClassifier): Fitted model (RF).
+        X_test (np.array): Predictors of test data. Shape(time, features)
+        y_test (np.array): Predictand of test data. Shape(time,)
+        beta (float): Weight for measure of weighted accuracy. (Default:0.5) 
+    Returns:
+        Set of metrics and figure of confusion matrix.
+    """
+    # Modules
+    from sklearn.metrics import confusion_matrix
+
+    # Confusion Matrix
+    y_test_pred = model.predict(X_test)
+    cf_matrix = confusion_matrix(y_test, y_test_pred)
+
+    # Calculate Metrics
+    tn, fp, fn, tp = cf_matrix.ravel()
+    tnr = tn / (tn + fp) # True negative rate
+    tpr = tp / (tp + fn) # True positive rate
+    gmean = np.sqrt(tnr * tpr) # G-Mean
+    beta = 0.5 # Weight
+    wacc = beta * tpr + (1 - beta) * tnr # Weighted Accuracy
+    precision = tp / (tp + fp) 
+    recall = tpr
+    fmeasure = (2 * precision * recall) / (precision + recall) # F-measure
+
+    # Display metrics
+    names = ["tnr", "tpr", "gmean", "wacc", "precision", "recall", "fmeasure"]
+    values = [tnr, tpr, gmean, wacc, precision, recall, fmeasure]
+    
+    print("Metric values \n")
+    for i, name in enumerate(names):
+        print(f"{name}: {values[i]}")
+
+    return (tnr, tpr, gmean, wacc, precision, recall, fmeasure)
---
+++ models/random_forest/rf002.py
@@ -0,0 +1,247 @@
+from matplotlib.pyplot import tight_layout
+
+def run(predictor="sp", percentile=0.95,): 
+    #---
+    #  Modules
+    #---
+    print("Import Modules\n")
+    import numpy as np
+    from data import preprocessing
+
+    #---
+    # Initialize
+    #---
+    folder = "results/random_forest/rf002/" # Where to save results
+
+    # ---
+    # Preprocessing
+    # ---
+
+    # Get timeseries of predictor and predictand
+    season = "winter"
+    
+    # Description of data
+    print("Description \n")
+    print("Model: Random Forest")
+    print("Predictand: Classification (0,1)")
+    print(f"Percentile Predictand: {percentile}")
+    print(f"Predictors: {predictor} ")
+    print(f"Seaon: {season}")
+    print("Preprocessing / Region: preprocessing1, only one station selected")
+
+    print("Start Preprocessing of Data\n\n")
+
+    X, Y, t = preprocessing.preprocessing1(season, predictor, percentile)
+
+    # Handle NaN values: 
+    # Insert numerical value that is not in data.
+    # ML will hopefully recognize it.
+    X[np.where(np.isnan(X))] = -999
+
+    # Save number of lat/lon for interpreting model output later
+    ndim = X.shape[0]
+    nlat = X.shape[1]
+    nlon = X.shape[2]
+
+    # Prepare shape for model
+    X = X.reshape(ndim, -1) # (ndim, nclasses)
+    y = Y[:, 0] # Select only one station
+
+    #---
+    # Train Model
+    #---
+    print("Start model training\n")
+    from sklearn.model_selection import train_test_split
+    from sklearn.ensemble import RandomForestClassifier
+
+    # Train-Test Split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.25)
+
+    # Setup Model
+
+    model = RandomForestClassifier(criterion='gini',
+    n_estimators=91, #- nTrees 
+    max_depth=9, 
+    random_state=0, # To compare results when changing hyperparameters
+    class_weight="balanced",
+    oob_score=True,
+    )
+
+    model.fit(X_train, y_train)
+
+    #---
+    # Evaluate model / Diagnostic
+    #--- 
+    print("Evaluate Model \n")
+
+    # Score & Importance
+    #---
+    test_score = model.score(X_test, y_test)
+    train_score = model.score(X_train, y_train)
+    importances = model.feature_importances_
+
+    print(f"test_score: {test_score}")
+    print(f"train_score: {train_score}")
+    print(f"importances: {importances}")
+
+    np.save(f"{folder}importances_{predictor}", importances)
+    print(f"saved importances to : {folder}importances_{predictor}")
+
+    # Confusion matrix
+    #---
+    # Format: 
+    # Reality / Model: Negative, Positive
+    # Negative    Right Negative, False Positive 
+    # Positive    False Negative, Right Positive
+
+    from sklearn.metrics import confusion_matrix
+    from models.evaluation import plot_cf
+
+    print("Show Confusion Matrix \n")
+
+    y_test_pred = model.predict(X_test)
+
+    cf_matrix = confusion_matrix(y_test, y_test_pred)
+
+    cfm_fig = plot_cf(cf_matrix)
+
+    cfm_fig.show()
+
+    fname = f"{folder}cf_matrix_{predictor}.jpg"
+    cfm_fig.savefig(fname)
+    print(f"saved cf matrix to : {fname}")
+
+
+    # AUROC
+    # Receiver Operating Characteristics & Area Under the Curve
+    #---
+    from sklearn.metrics import roc_curve, roc_auc_score
+    import matplotlib.pyplot as plt
+    
+    print("Show AUROC \n")
+
+    y_test_proba = model.predict_proba(X_test)[:, 1] # Prob. for predicting 0 or 1, we only need second col
+
+    fpr, tpr, thresholds = roc_curve(y_test, y_test_proba)
+    auc = roc_auc_score(y_test, y_test_proba)
+
+    print(f'AUC: {auc}')
+
+    fig, ax = plt.subplots(tight_layout=True)
+
+    ax.plot(fpr, tpr)
+    ax.set_xlabel("FPR")
+    ax.set_ylabel("TPR")
+    ax.set_title(f"AUROC with AUC = {auc}")
+    
+    fig.show()
+
+    fname = f"{folder}AUROC_{predictor}.jpg"
+    fig.savefig(fname)
+    print(f"saved AUROC to : {fname}")
+
+
+    print("End of Model run")
+
+def optimization(percentile=0.95):
+    """
+    Description:
+        Optimization of Hyperparameters applied to this run in order to get chosen hyperparameters in run().
+        For RandomForestClassifier Parameters:
+            n_estimators
+            max_depth
+            min_samples_leaf
+            min_samples_split
+    
+    Parameters:
+        percentile (float): Percentile of predictand data. (Defaults=0.95)
+
+    Returns:
+        Saves plots of validation curves for all predictors.
+    """
+    #---
+    # Optimization: Hyperparameters
+    #---
+    from data import preprocessing
+    # Get timeseries of predictor and predictand
+    predictors = ["sp", "tp", "u10", "v10",]
+    season = "winter"
+
+    # Hyperparameters to optimize
+    param_name = ('min_samples_leaf', "max_depth", "min_samples_split", "n_estimators",)
+    param_range = (np.arange(0, 10, 1), np.arange(2, 15, 1), np.arange(2, 25, 2), np.arange(0, 140, 20),)
+    hparams = dict(zip(param_name, param_range))
+
+    for predictor in predictors:
+        X, Y, t = preprocessing.preprocessing1(season, predictor, percentile)
+
+        # Handle NaN values: 
+        # Insert numerical value that is not in data.
+        # ML will hopefully recognize it.
+        X[np.where(np.isnan(X))] = -999
+
+        # Save number of lat/lon for interpreting model output later
+        ndim = X.shape[0]
+        nlat = X.shape[1]
+        nlon = X.shape[2]
+
+        # Prepare shape for model
+        X = X.reshape(ndim, -1) # (ndim, nclasses)
+        y = Y[:, 0] # Select only one station
+
+        #---
+        # Optimization: Hyperparameters with Validation Curve
+        #---
+
+        # Train-Test Split
+        from sklearn.ensemble import RandomForestClassifier
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.25)
+
+        from sklearn.model_selection import validation_curve
+        import numpy as np
+
+        for param_name, param_range in hparams.items():
+            train_scores, test_scores = validation_curve(
+            estimator=RandomForestClassifier(criterion="gini",
+            class_weight="balanced",
+            ),
+            X=X_train,
+            y=y_train,
+            param_name=param_name, #- varying for this parameter
+            param_range=param_range, #- for this parameters
+            cv=3,
+            )
+            
+            import matplotlib.pyplot as plt
+
+            fig, ax = plt.subplots(tight_layout=True)
+
+            ax.plot(param_range, np.mean(train_scores, axis=1), label='train')
+            ax.plot(param_range, np.mean(test_scores, axis=1), label='test-cv')
+            ax.set_title(f"Validation Curve for: {param_name}, Predictor: {predictor}")
+            ax.legend()
+
+            fig.show()
+
+            folder = "results/random_forest/rf002/"
+            fname = f"vc_{param_name}_{predictor}{str(percentile)[-2:]}"
+            fig.savefig(f"{folder}{fname}.jpg")
+
+        print("All Validation Curves saved")
+
+def save_hp(model_run="rf002", percentile=0.95):
+    """
+    Description: 
+        Saves Hyperparameters manually read from validation curves in 
+        master_thesis/results/random_forest/rf002.
+    """
+    param_name = ('min_samples_leaf', "max_depth", "min_samples_split", "n_estimators",)
+    param_range = (3, 13, 12, 100,)
+    params = dict(zip(param_name, param_range))
+
+    folder = "models/random_forest/hyperparameter/"
+    fname = f"{model_run}_{predictor}{str(percentile)[-2:]}.pkl"
+
+    with open(f"{folder}{fname}", 'wb') as f:
+        pickle.dump(params, f)
