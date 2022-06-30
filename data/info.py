736b366863075bbcee9460ec26140d39fcec092c
Kai Bellinghausen <bellinghausen.kai@gmail.com>
2022-06-30 13:35:02 +0200

    BUILD: updates data.saver module. <br> <br> Non-existant folders are created automatically. Duplicated filenames can be avoided.

--- data/info.py
+++ data/info.py
@@ -1,3 +1,8 @@
+#---
+# Description:
+#   Functions that give information on dataframes like duplicated indices, overlap of timeintervals etc.
+#---
+
 #- Modules
 import numpy as np
 
@@ -19,14 +24,12 @@ def get_duplicated_idx(df):
     else:
         return np.where(df.index.duplicated() == True)[0]
 
-def assert_timeintervals(t1, t2):
+def assert_equality(t1, t2):
     """
     Description: 
-        Checks if time intervals are the same. Throws assertion error if not.
+        Checks if two arrays are the same. Throws assertion error if not.
     Parameters:
-        t1, t2 (np.array): Arrays of time points, shape:(timepoints,)
-    Note: 
-        Can be used in general for two np.arrays
+        t1, t2 (np.array): Arrays of data points, shape:(datapoints,)
     """
 
     if len(np.where(t1 != t2)[0]) == 0:
@@ -34,4 +37,5 @@ def assert_timeintervals(t1, t2):
     else:
         is_equal = False
 
-    assert is_equal, "Timeinterval is not equal"
+    assert is_equal, "Arrays are not equal"
+    print("Arrays are the same")
--- data/saver.py
+++ data/saver.py
@@ -1,4 +1,4 @@
-def save_hp(hparams, hparam_names, predictor, model_run, percentile, folder):
+def save_hp(hparams, hparam_names, predictor, model_run, percentile, folder, is_overwrite=False):
     """
     Description: 
         Saves Hyperparameters manually read from validation curves in 
@@ -12,18 +12,33 @@ def save_hp(hparams, hparam_names, predictor, model_run, percentile, folder):
         model_run (str): Flag for model run.
         percentile (float): percentile of preprocessing predictand.
         folder (str): Folder where data is saved.
+        is_overwrite (bool): Whether to overwrite an existing file or create a new file. (Defaults: False)
     
     Returns:
         None
     """
+    # Modules
     import pickle
+
+    # Initialize
     params = dict(zip(hparam_names, hparams))
     fname = f"{model_run}_{predictor}{str(percentile)[-2:]}.pkl"
+    file_path = f"{folder}{fname}"
 
-    with open(f"{folder}{fname}", 'wb') as f:
+    # Check if folder exists, otherwise create it
+    directory_existance(folder)
+
+    # If files should not be overwritten, give unique name
+    if not is_overwrite:
+        file_path = unique_filenames(file_path)
+
+    # Save file
+    with open(f"{file_path}", 'wb') as f:
         pickle.dump(params, f)
+        print(f"File saved to: {file_path}")
 
-def save_hpdict(hparams, predictor, model_run, percentile, folder):
+
+def save_hpdict(hparams, predictor, model_run, percentile, folder, is_overwrite=False):
     """
     Description: 
         Saves Hyperparameters manually read from validation curves in 
@@ -36,12 +51,81 @@ def save_hpdict(hparams, predictor, model_run, percentile, folder):
         model_run (str): Flag for model run.
         percentile (float): percentile of preprocessing predictand.
         folder (str): Folder where data is saved.
+        is_overwrite (bool): Whether to overwrite an existing file or create a new file. (Default:False)
+
     
     Returns:
         None
     """
+    # Modules
     import pickle
-    fname = f"{model_run}_{predictor}{str(percentile)[-2:]}.pkl"
 
-    with open(f"{folder}{fname}", 'wb') as f:
+    # Initialize
+    fname = f"{model_run}_{predictor}{str(percentile)[-2:]}.pkl"
+    file_path = f"{folder}{fname}"
+
+    # Check if folder exists, otherwise create it
+    directory_existance(folder)
+
+    # If files should not be overwritten, give unique name
+    if not is_overwrite:
+        file_path = unique_filenames(file_path)
+    
+    # Save file
+    with open(f"{file_path}", 'wb') as f:
         pickle.dump(hparams, f)
+        print(f"File saved to: {file_path}")
+
+#---
+# Auxillary Functions
+#---
+
+def directory_existance(directory):
+    """
+    Description:
+        Checks whether a directory exists and creates a folder if it is non-existent.
+    Parameters:
+        directory (str): Path to a folder
+    Returns:
+        None
+    """
+
+    # Modules
+    import os
+
+    # Check if directory exists
+    is_exist = os.path.exists(directory)
+
+    # If directory does not exist, create it
+    if not is_exist:
+        os.makedirs(directory)
+        print(f"Created directory: {directory}")
+
+def unique_filenames(file_name):
+    """
+    Description:
+        Checks if a file already exists. If so, it gives a new name to the file by adding a number to the end.
+    Parameters:
+        file_name (str): Whole path to the file, e.g. notebooks/create_gifs.ipynb
+    Returns:
+        file_name (str): Adjusted path to the file, if file already existed, e.g. notebook/create_gifs1.ipynb
+
+    """
+    import os
+
+    if os.path.isfile(file_name):
+        expand = 0
+        while True:
+            expand += 1
+            fname = file_name.split(".")[0]
+            file_format = file_name.split(".")[1]
+            new_file_name = f"{fname}{str(expand)}.{file_format}"
+
+            if os.path.isfile(new_file_name):
+                continue
+            else:
+                file_name = new_file_name
+                break
+
+    return file_name
+        
