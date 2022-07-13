6c23f8c9dd626aee86a8e5b35cb912c77612b69a
Kai Bellinghausen <bellinghausen.kai@gmail.com>
2022-07-13 18:17:48 +0200

    BUILD: add loader module for models

---
+++ models/loader.py
@@ -0,0 +1,15 @@
+def load_model(model_path):
+    """
+    Description: 
+        Loads a saved model from disk
+    Parameters:
+        model_path (str): Path to modelfile (.pkl)
+    Returns:  
+        model (): classifier with fitted hyperparameters of the model run.
+    """
+    import pickle
+
+    with open(model_path, 'rb') as f:
+        model = pickle.load(f)
+
+    return model
